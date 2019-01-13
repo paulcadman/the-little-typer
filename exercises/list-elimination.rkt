50556b7de5d0f4f53490ba8c260529e67dc4698c
Paul Cadman <git@paulcadman.io>
2019-01-13 21:13:09 +0000

    Add exercises on Pi types and List elimination

---
+++ exercises/list-elimination.rkt
@@ -0,0 +1,55 @@
+#lang pie
+
+;; Exercises on Pi types and using the List elimiator from Chapters 4 and 5
+;; of The Little Typer
+;;
+;; Some exercises are adapted from assignments at Indiana University
+
+
+;; Exercise 4.1
+;;
+;; Extend the definitions of kar and kdr (frame 4.42) so they work with arbirary
+;; Pairs (instead of just for Pair Nat Nat).
+
+
+;; Exercise 4.2
+;;
+;; Define a function called compose that takes (in addition to the type
+;; arguments A, B, C) an argument of type (-> A B) and an argument of
+;; type (-> B C) that and evaluates to a value of type (-> A C), the function
+;; composition of the arguments.
+
+
+;; Exercise 5.1
+;;
+;; Define a function called sum-List that takes one List Nat argument and
+;; evaluates to a Nat, the sum of the Nats in the list.
+
+
+;; Exercise 5.2
+;;
+;; Define a function called maybe-last which takes (in addition to the type
+;; argument for the list element) one (List E) argument and one E argument and
+;; evaluates to an E with value of either the last element in the list, or the
+;; value of the second argument if the list is empty.
+
+
+;; Exercise 5.3
+;;
+;; Define a function called filter-list which takes (in addition to the type
+;; argument for the list element) one (-> E Nat) argument representing a
+;; predicate and one (List E) argument.
+;;
+;; The function evaluates to a (List E) consisting of elements from the list
+;; argument where the predicate is true.
+;;
+;; Consider the predicate to be false for an element if it evaluates to zero,
+;; and true otherwise.
+
+
+;; Exercise 5.4
+;;
+;; Define a function called sort-List-Nat which takes (in addition to the type
+;; argument for the list element) one (List E) argument and evaluates to a
+;; a (List E) consisting of the elements from the list argument sorted in
+;; ascending order.
