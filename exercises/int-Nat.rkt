da2477ec07f3cb593016f2a7de65bbda86948fc9
Paul Cadman <git@paulcadman.io>
2019-01-29 23:19:15 +0000

    Adds drop-last-k exercise.

--- exercises/int-Nat.rkt
+++ exercises/int-Nat.rkt
@@ -26,3 +26,12 @@
            [m Nat])
           (-> (Vec E n) (Vec E m)
               (Vec E (+ n m)))))
+
+;; Exercise 7.2
+;;
+;; Define a function called drop-last-k that takes an argument of type (Vec E ?) and
+;; evaluates to a value of type (Vec E ?), the result of dropping the last k elements
+;; from the first argument.
+;;
+;; NB: The type of the function should guarantee that we can't drop more elements
+;; than there are in the first argument.
