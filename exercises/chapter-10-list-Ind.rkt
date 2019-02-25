80b06cd4db7200b4d729aedc6420605766150442
Paul Cadman <git@paulcadman.io>
2019-02-25 23:11:35 +0000

    Add first exercise for chapter 10

---
+++ exercises/chapter-10-list-Ind.rkt
@@ -0,0 +1,63 @@
+#lang pie
+
+;; Exercises on ind-Nat from Chapter 10 of The Little Typer
+
+(claim +
+       (-> Nat Nat
+           Nat))
+
+(define +
+  (λ (a b)
+    (rec-Nat a
+             b
+             (λ (_ b+a-k)
+               (add1 b+a-k)))))
+
+(claim length
+       (Π ([E U])
+          (-> (List E)
+              Nat)))
+
+(define length
+  (λ (_)
+    (λ (es)
+      (rec-List es
+                0
+                (λ (_ _ almost-length)
+                  (add1 almost-length))))))
+
+(claim step-append
+       (Π ([E U])
+          (-> E (List E) (List E)
+              (List E))))
+
+(define step-append
+  (λ (E)
+    (λ (e es append-es)
+      (:: e append-es))))
+
+(claim append
+       (Π ([E U])
+          (-> (List E) (List E)
+              (List E))))
+
+(define append
+  (λ (E)
+    (λ (start end)
+      (rec-List start
+                end
+                (step-append E)))))
+
+;; Exercise 10.1
+;;
+;; Define a function called that states and proves that if you append
+;; two lists, l1 and l2, and then the length of the result is equal to
+;; the sum of the lengths of l1 and l2.
+
+(claim list-length-append-dist
+       (Π ([E U]
+           [l1 (List E)]
+           [l2 (List E)])
+          (= Nat
+             (length E (append E l1 l2))
+             (+ (length E l1) (length E l2)))))
