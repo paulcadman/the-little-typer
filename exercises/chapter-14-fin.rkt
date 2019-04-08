ad4a03a89b2109696f713788354a1c6269fafa74
Paul Cadman <git@paulcadman.io>
2019-04-08 23:46:50 +0100

    Adds exercise 14.1

---
+++ exercises/chapter-14-fin.rkt
@@ -0,0 +1,69 @@
+#lang pie
+
+;; Exercises on Fin from Chapter 14 of The Little Typer
+
+(claim Maybe
+       (-> U
+           U))
+
+(define Maybe
+  (λ (X)
+    (Either X Trivial)))
+
+(claim nothing
+       (Π ([E U])
+          (Maybe E)))
+
+(define nothing
+  (λ (_)
+    (right sole)))
+
+(claim just
+       (Π ([E U])
+          (-> E
+              (Maybe E))))
+
+(define just
+  (λ (_ e)
+    (left e)))
+
+(claim Fin
+       (-> Nat
+           U))
+
+(define Fin
+  (λ (n)
+    (iter-Nat n
+              Absurd
+              Maybe)))
+
+(claim fzero
+       (Π ([n Nat])
+          (Fin (add1 n))))
+
+(define fzero
+  (λ (n)
+    (nothing (Fin n))))
+
+(claim fadd1
+       (Π ([n Nat])
+          (-> (Fin n)
+              (Fin (add1 n)))))
+
+(define fadd1
+  (λ (n)
+    (λ (i-1)
+      (just (Fin n) i-1))))
+
+;; Exercise 14.1
+;;
+;; Define a function called insert-at that inserts a Nat into
+;; a Vector.
+;;
+;; The signature of the function should only allow calls that
+;; insert the Nat at an existing position in the Vector.
+
+(claim insert-at
+       (Π ([l Nat])
+          (-> (Fin (add1 l)) (Vec Nat l) Nat
+              (Vec Nat (add1 l)))))
